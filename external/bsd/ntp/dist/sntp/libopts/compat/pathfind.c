ab17468572a32296483c1199c55cb5a8bb7ca509
Kalvis Duckmanton <kalvisd@NetBSD.org>
2025-09-25 09:22:05 +1000

    Revert "ntp: (gcc14) appease gcc -Os"
    This reverts commit 950ab1799f245af84b1b36f58d20a5a7f5b9a49b.

--- external/bsd/ntp/dist/sntp/libopts/compat/pathfind.c
+++ external/bsd/ntp/dist/sntp/libopts/compat/pathfind.c
@@ -203,10 +203,7 @@ canonicalize_pathname( char *path )
         if (result[i] == '.') {
             /* Handle `./'. */
             if (result[i + 1] == '/') {
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wrestrict"
                 strcpy( result + i, result + i + 1 );
-#pragma GCC diagnostic pop
                 i = (start < 0) ? 0 : start;
                 continue;
             }
@@ -216,10 +213,7 @@ canonicalize_pathname( char *path )
                 (result[i + 2] == '/' || !result[i + 2])) {
                 while (--start > -1 && result[start] != '/')
                     ;
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wrestrict"
                 strcpy( result + start + 1, result + i + 2 );
-#pragma GCC diagnostic pop
                 i = (start < 0) ? 0 : start;
                 continue;
             }
